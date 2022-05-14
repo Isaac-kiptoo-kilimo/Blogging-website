18bda6ec18e1e724536fae539955c13272d11d1a
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-14 15:45:14 +0300

    feature:setting up the app

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv,find_dotenv
+
+load_dotenv(find_dotenv())
+
+class Config:
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:isaac@localhost/blogs'
+   
+
+class ProdConfig(Config):
+    # SQLALCHEMY_DATABASE_URI ='postgresql://mxhwjrztvelttj:f78c638ad02dbae50ef11ca5e8acab88b2bdc1a3ae0e1fa274ff3186213aa009@ec2-107-22-238-112.compute-1.amazonaws.com:5432/de50v5u7j3bip6'
+    pass
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:isaac@localhost/test_user'
+
+class DevConfig(Config):
+   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:isaac@localhost/blogs'
+   DEBUG = True
+
+
+config_options={
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test': TestConfig
+}
---
+++ start.sh
@@ -0,0 +1,8 @@
+export BLOG_API_KEY=''
+export SECRET_KEY='FlaskWTFSecretKey'
+export SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://moringa:isaac@localhost/blogs'
+export MAIL_USERNAME='isaac.kilimo@student.moringaschool.com'
+export MAIL_PASSWORD='0710443888Ik'
+
+
+python3.8 manage.py server
