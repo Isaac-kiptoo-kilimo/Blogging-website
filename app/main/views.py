1116bfa2a536b5c37ba40bcb39e6cff5c5ce5457
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-15 20:32:08 +0300

    feature:added the blogs

--- app/main/views.py
+++ app/main/views.py
@@ -5,31 +5,31 @@ from . import main
 from .. import db, login_manager
 from ..models import Blog, User,Comment
 from flask_login import  current_user ,login_required
-
+from ..email import mail_message
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-def make_blogs(blogs):
+# def make_blogs(blogs):
 
-  new_blogs = []
-  for blog in blogs:
-    user = User.query.filter_by(id=blog.user_id).first()
+#   new_blogs = []
+#   for blog in blogs:
+#     user = User.query.filter_by(id=blog.user_id).first()
    
-    comments = Comment.query.filter_by(blog_id=blog.id).all()
-    new_blog.append({
-      'id': blog.id,
-      'title': blog.title,
-      'author': blog.author,
-      'content': blog.content,
+#     comments = Comment.query.filter_by(blog_id=blog.id).all()
+#     new_blog.append({
+#       'id': blog.id,
+#       'title': blog.title,
+#       'author': blog.author,
+#       'content': blog.content,
       
-      'user': user,
-      'upvotes': blog.upvotes,
-      'downvotes': blog.downvotes,
-      'comments': len(comments)
-    })
-  return new_blogs
+#       'user': user,
+#       'upvotes': blog.upvotes,
+#       'downvotes': blog.downvotes,
+#       'comments': len(comments)
+#     })
+#   return new_blogs
 
 @main.route('/')
 def index():
@@ -53,10 +53,7 @@ def addblog():
     flash('blog created successfully',"success")
     users=User.query.all()
     for user in users:
-      email=user['email']
-      subject='We have posted a new blog post'
-      content=f'Here is the title {title}'
-      
+      mail_message("Welcome to pitches app","email/welcome_user",user.email,user=user)
     return redirect(url_for('main.index'))
   return render_template ('pages/blogs/addblog.html')
    
@@ -68,4 +65,18 @@ def view_blog(blog_id):
     
   blogs=Blog.query.all()
   return render_template('pages/blogs/view.html',blog=blog,quote1=quotes[0],quote2=quotes[2],blogs=blogs,quote3=quotes[3],quote4=quotes[4])
-
+@main.route('/')
+def add_comment(blog_id):
+  blog = Blog.query.filter_by(id=blog_id).first()
+  if request.method == 'POST':
+    content = request.form['comment']
+    if blog:
+      comment = Comment(blog_id=blog.id, user_id=current_user.id, content=content)
+      db.session.add(comment)
+      db.session.commit()
+      flash('Comment added', 'success')
+      return redirect(url_for('main.view_blog', blog_id=blog.id))
+    else:
+      flash('Pitch not found', 'warning')
+      return redirect(url_for('main.index'))
+  return redirect(url_for('main.view_blog', blog_id=blog.id))
