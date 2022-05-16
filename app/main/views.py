7c696930a722e5cab4ac5bc4afccf3c7ef509b88
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-16 08:42:18 +0300

    fix:fixing the comments section

--- app/main/views.py
+++ app/main/views.py
@@ -69,8 +69,24 @@ def view_blog(blog_id):
   return render_template('pages/blogs/view.html',blog=blog,quote1=quotes[0],quote2=quotes[2],blogs=blogs,quote3=quotes[3],quote4=quotes[4])
 
 
-@main.route('/blogs/view/<int:blog_id>', methods=['GET','POST'])
-def add_comment(blog_id):
+# @main.route('/comments/blogs/add/<int:blog_id>', methods=['GET','POST'])
+# def add_comment(blog_id):
+#   blog = Blog.query.filter_by(id=blog_id).first()
+#   if request.method == 'POST':
+#     content = request.form['comment']
+#     if blog:
+#       comment = Comment(blog_id=blog.id, user_id=current_user.id, content=content)
+#       db.session.add(comment)
+#       db.session.commit()
+#       flash('Comment added', 'success')
+#       return redirect(url_for('main.view_blog', blog_id=blog.id))
+#     else:
+#       flash('blog not found', 'warning')
+#       return redirect(url_for('main.index'))
+#   return render_template ('pages/blogs/addcomment.html',comment=comment)
+#   # return redirect(url_for('main.add_comment', blog_id=blog.id))
+@main.route('/comments/add/<string:blog_id>',methods=['GET','POST'])
+def comment_view(blog_id):
   blog = Blog.query.filter_by(id=blog_id).first()
   if request.method == 'POST':
     content = request.form['comment']
@@ -78,9 +94,11 @@ def add_comment(blog_id):
       comment = Comment(blog_id=blog.id, user_id=current_user.id, content=content)
       db.session.add(comment)
       db.session.commit()
+      blog_id=blog.id
       flash('Comment added', 'success')
-      return redirect(url_for('main.view_blog', blog_id=blog.id))
+      return redirect(url_for('main.view_blog', blog=blog))
     else:
-      flash('Pitch not found', 'warning')
+      flash('blog not found', 'warning')
       return redirect(url_for('main.index'))
-  return redirect(url_for('main.view_blog', blog_id=blog.id))
+  
+  return render_template('pages/blogs/addcomment.html',blog=blog)
